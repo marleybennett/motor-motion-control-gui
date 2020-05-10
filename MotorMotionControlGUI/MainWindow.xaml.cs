caa3890591546bafade8ed71ca1cfff29042dd1d
Marley Bennett <marleybennett98@gmail.com>
2020-05-10 13:39:16 -0700

    initial commit pls work

--- MotorMotionControlGUI/MainWindow.xaml.cs
+++ MotorMotionControlGUI/MainWindow.xaml.cs
@@ -22,16 +22,17 @@ namespace MotorMotionControlGUI
         struct Parameter
         {
             private readonly string name;
-            private readonly int min;
-            private readonly int max;
-            private readonly int defaultVal;
+            private readonly float min;
+            private readonly float max;
+            private readonly float defaultVal;
             private readonly string units;
-            public int currentVal;
+            public float currentVal;
+            private readonly string additionalDetails;
             private TextBox tbInput;
             private Button button;
             private TextBlock tbDescription;
 
-            public Parameter(string name, int min, int max, int defaultValue, string units, TextBox tbInput, Button button, TextBlock tbDescription)
+            public Parameter(string name, float min, float max, float defaultValue, string units, TextBox tbInput, Button button, TextBlock tbDescription, string additionalDetails = null)
             {
                 this.name = name;
                 this.min = min;
@@ -39,6 +40,7 @@ namespace MotorMotionControlGUI
                 this.defaultVal = defaultValue;
                 this.currentVal = defaultValue;
                 this.units = units;
+                this.additionalDetails = additionalDetails;
                 this.tbInput = tbInput;
                 this.button = button;
                 this.tbDescription = tbDescription;
@@ -46,11 +48,12 @@ namespace MotorMotionControlGUI
             }
 
             public string Name { get { return name; } }
-            public int Min { get { return min; } }
-            public int Max { get { return max; } }
-            public int DefaultVal { get { return defaultVal; } }
-            public int CurrentVal { get { return currentVal; } }
+            public float Min { get { return min; } }
+            public float Max { get { return max; } }
+            public float DefaultVal { get { return defaultVal; } }
+            public float CurrentVal { get { return currentVal; } }
             public string Units { get { return units; } }
+            public string AdditionalDetails { get { return additionalDetails; } }
             public TextBox TbInput { get { return tbInput; } }
             public Button Button { get { return button; } }
             public TextBlock TbDescription { get { return tbDescription; } }
@@ -60,27 +63,60 @@ namespace MotorMotionControlGUI
         Parameter[] paramArr;
         int numParameters = 3;
 
-
+        /*****************
+         * NAME: UpdateDescription
+         * DESCRIPION: Update parameter description with current value.
+         * ***************/
         private void UpdateDescription(Parameter p)
         {
-            p.TbDescription.Text = p.Name + "\n" + "Range: " + p.Min + " - " + p.Max + "\nCurrent Value: " + p.CurrentVal;
+            p.TbDescription.Text = p.Name + "\n" + "Range: " + p.Min + " - " + p.Max + " " + p.Units + "\nCurrent Value: " + p.CurrentVal + " " + p.Units + "\n" + p.AdditionalDetails;
         }
 
+        /*****************
+         * NAME: InitializeTextBox
+         * DESCRIPION: Set up textbox corresponding to parameter value.
+         * ***************/
         private void InitializeTextBox(Parameter p)
         {
             p.TbInput.Tag = p.Name;
             p.TbInput.Foreground = Brushes.Gray;
-            p.TbInput.Text = "Default " + p.DefaultVal;
+            p.TbInput.Text = "Default: " + p.DefaultVal;
             p.TbInput.BorderBrush = Brushes.Black;
             p.TbInput.AcceptsReturn = false;
+            p.TbInput.BorderBrush = osuBrush;
             p.Button.Tag = p.Name;
             UpdateDescription(p);
-            p.TbInput.BorderBrush = osuBrush;
         }
 
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+           // Initialize array of parameter structs
+           // All parameter values goes here in form: parameterName, minParameterValue, maxParameterValue, unitName, inputTextBoxName, submitButtonName, descriptionTextBoxName
+           // Use "f" in front of decimal values to ensure conversion to float (rather than double) for processing with microcontroller
+           paramArr = new Parameter[]
+            {
+                new Parameter("Position", 0, 365, 180, "degrees", text1, button1, description1),
+                new Parameter("Speed", 0, 20, 10.23f, "m/s", text2, button2, description2),
+                new Parameter("Param3", 0, 100, 3, "m/h", text3, button3, description3),
+
+            };
+
+            // Initialize textbox for each parameter
+            for (int i = 0; i < numParameters; i++)
+                InitializeTextBox(paramArr[i]);
+
+        }
+
+        /*****************
+         * NAME: GetParameter
+         * DESCRIPION: Return parameter based on input tag
+         * ***************/
         private Parameter GetParameter(string tag)
         {
-            for(int i = 0; i < numParameters; i++)
+            for (int i = 0; i < numParameters; i++)
             {
                 if (paramArr[i].Name == tag)
                     return paramArr[i];
@@ -89,6 +125,10 @@ namespace MotorMotionControlGUI
             return paramArr[0];
         }
 
+        /*****************
+         * NAME: GetParameter
+         * DESCRIPION: Return parameter based on input button
+         * ***************/
         private Parameter GetParameter(Button button)
         {
             for (int i = 0; i < numParameters; i++)
@@ -100,68 +140,76 @@ namespace MotorMotionControlGUI
             return paramArr[0];
         }
 
-        public MainWindow()
+        /*****************
+         * NAME: CheckLimits
+         * DESCRIPION: Check if input value is within limits.
+         * Return 0 if invalid input.
+         * Return integer value of input if valid.
+         * ***************/
+        private float CheckLimits(Parameter p, string value)
         {
+            float valueFloat;
 
-            InitializeComponent();
+            if (!float.TryParse(value, out valueFloat))
+                return 0;
 
-           paramArr = new Parameter[]
-            {
-                new Parameter("Angle", 0, 100, 1, "degrees", text1, button1, description1),
-                new Parameter("Speed", 0, 100, 2, "m/s", text2, button2, description2),
-                new Parameter("Ramp", 0, 100, 3, "??", text3, button3, description3),
+            if (valueFloat < p.Min || valueFloat > p.Max)
+                return 0;
 
-            };
+            return valueFloat;
+        }
 
-            for (int i = 0; i < numParameters; i++)
-                InitializeTextBox(paramArr[i]);
+        private void SendViaSerial(Parameter p)
+        {
 
         }
 
 
-        private int CheckLimits(Parameter p, string value)
-        {
-            int valueInt;
-
-            if (!int.TryParse(value, out valueInt))
-                return 0;
-
-            if (valueInt < p.Min || valueInt > p.Max)
-                return 0;
-
-            return valueInt;
-        }
-
-        void SubmitValue(string tag)
+        /*****************
+         * NAME: SubmitValue
+         * DESCRIPION: Call SubmitValue with Parameter based on tag value.
+         * ***************/
+        private void SubmitValue(string tag)
         {
             SubmitValue(GetParameter(tag));
         }
 
-
-        void SubmitValue(Parameter p)
+        /*****************
+         * NAME: SubmitValue
+         * DESCRIPION: Check if input value is valid
+         * If valid, update description and send value to motor.
+         * ***************/
+        private void SubmitValue(Parameter p)
         {
-
+            // Get value from textbox
             string value = p.TbInput.Text;
-            string tag = p.TbInput.Tag.ToString();
 
-            int validInt = CheckLimits(p, value);
+            // Check if value is valid
+            float validFloat = CheckLimits(p, value);
 
-            if (validInt != 0)
+            // Valid
+            if (validFloat != 0)
             {
-                MessageBox.Show("Updated " + tag + " with new value of " + value);
-                p.currentVal = validInt;
+                MessageBox.Show("Updated " + p.TbInput.Tag.ToString() + " with new value of " + value);
+                p.currentVal = validFloat;
                 UpdateDescription(p);
+                SendViaSerial(p);
             }
 
+            // Invalid
             else
             {
                 MessageBox.Show("Invalid parameter value.");
             }
 
-            p.TbInput.Text = "Default " + p.DefaultVal;
+            p.TbInput.Text = "Default: " + p.DefaultVal;
             p.TbInput.Foreground = Brushes.Gray;
         }
 
+        /*****************
+         * NAME: GetTextBox
+         * DESCRIPION: Submit value based on button input
+         * ***************/
         private void GetTextBox(object sender, RoutedEventArgs e)
         {
             Parameter p = GetParameter(sender as Button);
@@ -169,6 +217,11 @@ namespace MotorMotionControlGUI
         }
 
 
+        /*****************
+         * NAME: CheckEnter
+         * DESCRIPION: If enter key pressed, submit value.
+         * If any other key was pressed, clear "Default" text.
+         * ***************/
         private void CheckEnter(object sender, System.Windows.Input.KeyEventArgs e)
         {
             TextBox tb = sender as TextBox;
